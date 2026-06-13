f299904a93bf68859ddb3e4290b16351c0001ff4
kumar-art <vadlamurikumar18@gmail.com>
2026-06-13 16:00:01 +0530

    added the some changes

--- src/test/java/Runner/TestRunnerTest.java
+++ src/test/java/Runner/TestRunnerTest.java
@@ -27,7 +27,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/api_test.feature",
+       // features = "src/test/resources/features/api_test.feature",
+        features = "src/test/resources/features/login.feature",
         glue = "StepDefinitions",
         plugin = {
                 "pretty",
